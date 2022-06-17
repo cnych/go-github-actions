c78d16be019df0f1213b2eb6eec1faf9d2b1043d
lishuo <lishuo@mesomia-tech.com>
2022-06-17 18:25:35 +0800

    [feature: add param]

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGreet(t *testing.T) {
 	result := Greet()
-	if result != "Hello GitHub Actions. qikqiak.com is awesome fwef" {
+	if result != "Hello GitHub Actions. qikqiak.com is awesomefwef" {
 		t.Errorf("Greet() = %s; Expected Hello GitHub Actions. qikqiak.com is awesome fwef", result)
 	}
 
